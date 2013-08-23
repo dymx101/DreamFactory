8c93c62767ea60a2b503a90977bd8219dbeb7700
dymx101 <dongyiming@Dongs-MacBook-Pro.local>
2013-08-23 11:21:32 +0800

    create project

---
+++ DreamFactory/DreamFactory/TMDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TMDAppDelegate.h
+//  DreamFactory
+//
+//  Created by Dong Yiming on 8/23/13.
+//  Copyright (c) 2013 ToMaDon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DreamFactory/DreamFactory/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DreamFactory
+//
+//  Created by Dong Yiming on 8/23/13.
+//  Copyright (c) 2013 ToMaDon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TMDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TMDAppDelegate class]));
+    }
+}
